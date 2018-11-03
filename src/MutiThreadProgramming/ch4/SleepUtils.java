b8b78269bfda39c3a61ceae2c8b668be3af930b5
Yan <1757901534@qq.com>
2018-11-03 10:54:27 +0800

    工具类

---
+++ src/MutiThreadProgramming/ch4/SleepUtils.java
@@ -0,0 +1,12 @@
+package MutiThreadProgramming.ch4;
+
+import java.util.concurrent.TimeUnit;
+
+public class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
