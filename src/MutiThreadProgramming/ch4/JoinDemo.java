f249a5664eb630f8397d528cf0ca4c44f2bb9f18
Yan <1757901534@qq.com>
2018-11-27 15:27:06 +0800

    join测试

---
+++ src/MutiThreadProgramming/ch4/JoinDemo.java
@@ -0,0 +1,53 @@
+package MutiThreadProgramming.ch4;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Yan_Jiang on 2018/11/27.
+ * Thread.join()测试
+ */
+public class JoinDemo {
+
+    public static void main(String[] args) throws Exception {
+        Thread previous = Thread.currentThread();
+        for (int i = 0; i < 10; i++) {
+            // 每个线程拥有前一个线程的引用，需要等待前一个线程终止，才能从等待中返回
+            Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+            thread.start();
+            previous = thread;
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(Thread.currentThread().getName() + " terminate.");
+    }
+
+    static class Domino implements Runnable {
+        private Thread thread;
+
+        public Domino(Thread thread) {
+            this.thread = thread;
+        }
+
+        public void run() {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+            }
+            System.out.println(Thread.currentThread().getName() + " terminate.");
+        }
+    }
+}
+   /** 运行结果
+        main terminate.
+        0 terminate.
+        1 terminate.
+        2 terminate.
+        3 terminate.
+        4 terminate.
+        5 terminate.
+        6 terminate.
+        7 terminate.
+        8 terminate.
+        9 terminate.
+    */
+
